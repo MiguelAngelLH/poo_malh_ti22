f4c7ff760994ec5d7e9e4da7054d3d8c29cd8d84
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2023-01-24 14:56:14 +0000

    programa que suma , resta, multiplica y divide con la forma format

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 |1|programa0.py|hola mundo en python3|
 |2|programa1.py|concatenacion y comentarios multimedia.py|
 |3|progama2.py|concatenacion mediante la funcion format.py
+|4|progama3.py|concatenacion mediante la funcion format para realizar suma,resta,multiplicacion,division con todo y el resultado.|
--- semana2/programa3.py
+++ semana2/programa3.py
@@ -3,3 +3,9 @@ variable2 = 5
 print(variable1+variable2)
 print ("{} + {} = {}".format (variable1,variable2,variable1+variable2
 ))
+print ("{} - {} = {}".format (variable1,variable2,variable1-variable2
+))
+print ("{} / {} = {}".format (variable1,variable2,variable1/variable2
+))
+print ("{} = {} * {}".format (variable2*variable1,variable1,variable2
+))
