b889b828a15337839989df4f8685feeed8ef637e
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2023-01-31 14:33:00 +0000

    actividad para calcular el área de un circulo

---
+++ semana3/programa6.py
@@ -0,0 +1,9 @@
+import math
+
+R=float(input("Escribe el radio:"))
+circunferencia=2*math.pi*R
+area=math.pi*R*R
+superficiearea=4*math.pi*R*R
+print ( "Circunferencia:% .2f" % circunferencia)
+print ( "Area del circulo:% .2f"% area)
+print ( "Area de superficie del circulo:% .2f"% superficiearea)
