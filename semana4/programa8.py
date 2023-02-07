9f7c825c5c1d811cf7e10e029e05a318e3be190a
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2023-02-07 14:54:57 +0000

    11 formas diferentes de comparar 2 valores

---
+++ semana4/programa8.py
@@ -0,0 +1,53 @@
+n1= int(input("n1"))
+n2= int(input("n2"))
+if n2<n1:
+   print(n1)
+if n1<n2:
+  print(n2)
+if n1==n2:
+  print(None)
+
+if n2>n1:
+   print(n2)
+if n1>n2:
+  print(n1)
+if n2==n1:
+  print(None)
+
+if n1>n2:
+  print(n1)
+elif n2>n1:
+  print(n2)
+else:
+  print(None)
+
+if n1>n2:
+  print(n2)
+elif n2>n1:
+  print(n1)
+else:
+  print(None)
+
+if n2<n1:
+   print(n1)
+if n1<n2:
+  print(n2)
+if n2==n1:
+  print(None)
+
+if n2>=n1:
+ if n1>n2:
+   print(n2)
+else:
+  print(None)
+else: 
+   print(n2)
+
+if n1<=n2:
+ if n1<n2:
+   print(None)
+else:
+  print(None)
+else:
+   print(n1)
+
