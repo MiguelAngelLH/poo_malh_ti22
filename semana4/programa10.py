7a5b4feb6ac7e2838437f406c650518a35a64ea5
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2023-02-08 14:54:55 +0000

    se creo el programa9 que idenfica el numero mayor

---
+++ semana4/programa10.py
@@ -0,0 +1,11 @@
+def  mayor(numero1:int,numero2:int):
+       mayor=None
+ if numero1 > numero2:
+   mayor = (numero1)
+ elif numero2>numero1:
+  mayor=(numero2)
+ else: 
+   mayor= None
+ return mayor
+
+     
---
+++ semana4/programa9.py
@@ -0,0 +1,18 @@
+
+def mayor(numero1,numero2):
+
+    if numero1>numero2:
+      print("el numero1 es mayor :{}".format(numero1))
+    elif numero2>numero1:
+      print("el numero2 es mayor :{}".format(numero2))
+
+    else:
+     print("iguales")
+
+    mayor(3,2)
+    mayor(2,3)
+    mayor(3,3)
+   
+
+   
+   
